06b466537b0087f7a87fbfe197525128e475d45e
jack <1594250583@qq.com>
2020-07-26 20:25:17 +0800

    update

---
+++ flower/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+
+}
